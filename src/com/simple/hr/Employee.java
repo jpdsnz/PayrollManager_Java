3087f4442e867b6f2f20831343c8af79a9063100
jpdsnz <57234105+jpdsnz@users.noreply.github.com>
2022-11-21 12:30:26 -0600

    Payroll Management System - Added feature to group employees by their department

--- src/com/simple/hr/Employee.java
+++ src/com/simple/hr/Employee.java
@@ -1,16 +1,17 @@
 package com.simple.hr;
 
 public abstract class Employee implements Payable{
-
+    //---- Variables ----//
     private String name;
 
     private double payRate;
     private final int EMPLOYEE_ID;
     private Address address;
-    private Department department;
+    protected Department department;
     private static int nextID = 1000;
     public static double STARTING_PAY_RATE = 7.75;
 
+    //---- Constructors ----//
     public Employee(String name){
         this.name = name;
         EMPLOYEE_ID = getNextID();
@@ -23,12 +24,14 @@ public abstract class Employee implements Payable{
         EMPLOYEE_ID = getNextID();
         payRate = STARTING_PAY_RATE;
     }
-
+    //---- Methods ----//
     public String getName(){ return name; }
 
     public int getEmployeeID(){ return EMPLOYEE_ID; }
 
     public double getPayRate(){ return payRate; }
+
+    public String getDepartment() { return department.getName(); }
 
     public void changeName(String newName) { name = newName; }
 
@@ -43,4 +46,11 @@ public abstract class Employee implements Payable{
         nextID++;
         return id;
     }
+
+    //Override the toString Method!
+    @Override
+    public String toString(){
+        return this.name; //+ " - " + this.getDepartment();
+    }
+
 }
--- src/com/simple/hr/Main.java
+++ src/com/simple/hr/Main.java
@@ -1,24 +1,69 @@
 package com.simple.hr;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
-        SalariedEmployee emp1 = new SalariedEmployee("Bob Sawyer", 65000);
-        SalariedEmployee emp2 = new SalariedEmployee("Jane White", 57000);
-        SalariedEmployee emp3 = new SalariedEmployee("Joe Brown", 50000);
+        //Department Objects
+        Department dept1 = new Department("Accounting");
+        Department dept2 = new Department("IT");
+        Department dept3 = new Department("Sales");
 
+        //Department List
+        List<Department> departments = new ArrayList<>();
+
+        //Address Object
+        Address address = new Address("5 Emerald close", "Kingston", "Kingston", "00000");
+
+        //Salaried Employees
+        SalariedEmployee emp1 = new SalariedEmployee("Bob Sawyer", 65000, dept1);
+        SalariedEmployee emp2 = new SalariedEmployee("Jane White", 57000, dept2);
+        SalariedEmployee emp3 = new SalariedEmployee("Joe Brown", 50000, dept3);
+        SalariedEmployee emp4 = new SalariedEmployee("Sarah Thomas", 50000, dept3);
+        SalariedEmployee emp5 = new SalariedEmployee("Alex Eucker", 40000, dept2);
+
+        //Array for employees
+        Employee[] employees = {emp1, emp2, emp3,emp4, emp5};
+
+        //List for printing employee info
+        List<Employee> employeeList =  Arrays.asList(employees);
+
+        //Print Stream (Not Grouped by dept)
+        //employeeList.stream().forEach(System.out::println); //Use scope resolution operator since dealing with stream
+
+        // Group Employees by dept
+        System.out.println("Employees grouped by Department");
+        Map<String, List<Employee>> groupByDepartment =
+                employeeList.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+        groupByDepartment.forEach(
+                (department, employeesInDepartment) -> //Lamda function
+                {
+                    System.out.println(department);
+                    employeesInDepartment.forEach(
+                            employee -> System.out.printf("  %s%n", employee)
+                    );
+                }
+        );
+
+        //File name
         String fileName = "Paystubs.txt";
 
+
+        /*
         List<SalariedEmployee> employees = new ArrayList<>();
         employees.add(emp1);
         employees.add(emp2);
         employees.add(emp3);
-
         SalariedEmployee.printStub(fileName, employees);
+        */
+
+
 
         //HourlyEmployee emp2 = new HourlyEmployee("Jane White", 35);
         //System.out.println(emp1.calculatePay());
